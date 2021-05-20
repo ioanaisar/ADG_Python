779db2c25514583140bc3dbd0ca81a187e2fd81c
ioanateodora <ioanaisar0@gmail.com>
2021-05-20 08:33:07 +0300

    First commit

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+    copy_list = sorted(my_list)
+    copy_list_reversed = sorted(my_list, reverse=True)
+    even_list = copy_list[1::2]
+    odd_list = copy_list[::2]
+    multiple_list = copy_list[2::3]
+    print(my_list)
+    print(copy_list)
+    print(copy_list_reversed)
+    print(even_list)
+    print(odd_list)
+    print(multiple_list)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
